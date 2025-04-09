a656e1be5f66b8711f16a3ce389304c3bc3de7af
LostOxygen <jonathan.evertz@gmail.com>
2025-04-09 11:42:37 +0200

    added experiment ideas

---
+++ pitfall_11.py
@@ -0,0 +1,2 @@
+# Use Devign dataset and change its distribution from 50/50 to more like
+# # 90/10 or 95/5 and see if the model (training and evaluation) still works
---
+++ pitfall_5.py
@@ -0,0 +1,3 @@
+# Use one of Niklas' vulnerability detection LLMs 
+# and perform a simple experiment using two sizes of context 
+# one large enough and one obviously too small
